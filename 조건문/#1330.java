04c18d1198594e0057b87dc66a35c6d33db54861
You Jeong Jang <sunny819819@naver.com>
2023-09-25 16:31:24 +0900

    Create #1330.java

---
+++ 조건문/#1330.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        if(A>B){
+            System.out.println(">");
+        } else if (A<B) {
+            System.out.println("<");
+        }else{
+            System.out.println("==");
+        }
+    }
+}
