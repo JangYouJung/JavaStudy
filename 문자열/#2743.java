ce23a55df21ccd4146660edb680fcc6abffbad27
You Jung <80906691+JangYouJung@users.noreply.github.com>
2023-10-04 23:39:06 +0900

    Update #2743.java
    주석 추가

--- 문자열/#2743.java
+++ 문자열/#2743.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String word;
-
         Scanner in = new Scanner(System.in);
         word = in.next();
-
-        System.out.println(word.length());
+        System.out.println(word.length());//java에서는 string의 특정 인덱스에 접근할 때, 'length()'메서드를 사용한다!
     }
 }
