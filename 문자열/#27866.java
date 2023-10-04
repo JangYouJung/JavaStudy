36fe598dd11670946003e299cca243053ec43a93
You Jung <80906691+JangYouJung@users.noreply.github.com>
2023-10-04 23:44:23 +0900

    Update #27866.java

--- 문자열/#27866.java
+++ 문자열/#27866.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String word;
-        int index;
+        //1. 변수 선언
+        String word; //단어 저장
+        int index; // 인덱스 저장
 
+        //2. 각 변수 입력 받기
         Scanner in = new Scanner(System.in);
-        word = in.next();
+        word = in.next(); 
         index = in.nextInt();
 
+        //3. 단어의 '인덱스 - 1'번째 문자 출력
         System.out.println(word.charAt(index-1));
     }
 }
