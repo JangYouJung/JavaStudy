edc7f50bf291085b90ff9f45d8ec805d424cc01f
You Jeong Jang <sunny819819@naver.com>
2023-10-04 23:30:23 +0900

    Create #2743.java

---
+++ 문자열/#2743.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String word;
+
+        Scanner in = new Scanner(System.in);
+        word = in.next();
+
+        System.out.println(word.length());
+    }
+}
