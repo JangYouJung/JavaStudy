6014eac42e6ef9123b268aa6c077ab58136b6e06
You Jeong Jang <sunny819819@naver.com>
2023-10-27 21:56:01 +0900

    Create #24264_시간복잡도3_Failed.java

---
+++ 시간복잡도/#24264_시간복잡도3_Failed.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n*n);
+        System.out.println(2);
+    }
+}
