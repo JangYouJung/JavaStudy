8f03023f17131af9a878f3bbb15399642a8d7d19
You Jeong Jang <sunny819819@naver.com>
2023-11-06 22:35:16 +0900

    Create #2742 기찍.java

---
+++ 23-2 성신 컴공 학술제 코딩테스트/#2742 기찍.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        while(N>0){
+            System.out.println(N);
+            N--;
+        }
+    }
+}
