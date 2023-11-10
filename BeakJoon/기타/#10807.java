ce0f3c508ae3bfbc441d745d8831219340e4ab26
You Jeong Jang <sunny819819@naver.com>
2023-11-10 23:58:34 +0900

    Create #10807.java

---
+++ BeakJoon/기타/#10807.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // 정수 개수 입력 받기
+
+        Vector<Integer> array = new Vector<>();
+        for(int i = 0; i < n; i++){ //벡터 채우기
+            int a = in.nextInt();
+            array.add(a);
+        }
+
+        int k = in.nextInt();
+        int answer = 0;
+        for(int i = 0; i<n; i++){
+            if(k==array.elementAt(i)) answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
